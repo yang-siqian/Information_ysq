8a84463ef40419a0c620714128bcc8d9f2e72767
杨思倩 <siqian20150501@163.com>
2018-08-17 01:06:54 +0800

    集成数据库迁移命令

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
+# 指定session保存指定位置
 from flask_session import Session
 from flask_wtf import CSRFProtect
 from redis import StrictRedis
 from flask import Flask,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
 
 
 class Config(object):
@@ -48,6 +50,10 @@ CSRFProtect(app)
 Session(app)
 
 manager = Manager(app)
+# 将app与db关联
+Migrate(app,db)
+# 将迁移命令添加到manager中
+manager.add_command('db',MigrateCommand)
 
 @app.route("/")
 def index():
