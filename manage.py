8e41433d3a413c9e07747f25534c64983b2c30fe
杨思倩 <siqian20150501@163.com>
2018-08-16 21:15:36 +0800

    项目的配置创建&数据库的配置

--- manage.py
+++ manage.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql416@127.0.0.1:3306/information_ysq"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
 
+# 从app加载配置
+app.config.from_object(Config)
 
-
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 
@@ -13,4 +24,4 @@ def index():
     return "index22233"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
