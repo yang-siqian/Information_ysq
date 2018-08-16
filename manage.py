f1c81179b7d93ecd4fd8a0d58b49499e8b1e723e
杨思倩 <siqian20150501@163.com>
2018-08-17 01:14:00 +0800

    抽取项目配置到单独的配置文件

--- manage.py
+++ manage.py
@@ -7,29 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
 
-
-class Config(object):
-    """项目的配置"""
-    DEBUG = True
-    SECRET_KEY ='Kx5jPQ5OAsxkNhOd+byp+mcTpiTpo9hS/YzHj6XuiW2woldfk1J8l5vJqhRG3tzm'
-    # 为数据库添加配置
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql416@127.0.0.1:3306/information_ysq"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # 为Redis添加配置
-    REDIS_HOST = "127.0.0.1"
-    REDIS_PORT = 6379
-
-    # session保存配置
-    SESSION_TYPE = "redis"
-    # 开启session签名
-    SESSION_USE_SIGNER = False
-    # 指定session保存的redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
-    # 设置需要过期
-    SESSION_PERMANENT = False
-    # 设置过期时间2天
-    PERMANENT_SESSION_LIFETIME = 86400*2
+from config import Config
 
 
 app = Flask(__name__)
