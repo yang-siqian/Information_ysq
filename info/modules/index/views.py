409966d1fc998bc1af672bb7bba82361ddf91560
杨思倩 <siqian20150501@163.com>
2018-08-19 09:00:40 +0800

    加载favicon.ico小图标

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,6 +1,8 @@
 # import logging
 # from flask import current_app
-from info import redis_store
+# from info import redis_store
+from flask import render_template, current_app
+
 from . import index_blue
 
 @index_blue.route("/")
@@ -12,5 +14,9 @@ def index():
     # logging.warning('测试warning')
     # logging.fatal('测试fatal')
     # current_app.logger.error('测试error')
-    redis_store.set('name','laowang')
-    return "index22233"
+    # redis_store.set('name','laowang')
+    return render_template('news/index.html')
+
+@index_blue.route("/favicon.ico")
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
