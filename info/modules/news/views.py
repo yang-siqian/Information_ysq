111b0b134b0f47229033e915d47004f61ecc707e
杨思倩 <siqian20150501@163.com>
2018-08-22 23:52:44 +0800

    模板抽取&详情页基类模板展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,8 @@
+from info import constants
+from info.models import News
 from info.modules.news import news_blue
-from flask import render_template
+from flask import render_template, current_app
+
 
 @news_blue.route('/<int:news_id>')
 def news_detail(news_id):
@@ -8,4 +11,18 @@ def news_detail(news_id):
     :param news_id:
     :return:
     """
-    return render_template("news/detail.html")
+
+    # 右侧新闻排行的逻辑实现
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+    news_dict_li = []
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
+
+    data = {
+        "news_dict_li":news_dict_li
+    }
+    return render_template("news/detail.html",data=data)
