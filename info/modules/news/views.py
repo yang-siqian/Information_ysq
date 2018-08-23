f5afe3f4869dcea90562825fc76c4d1105ecccff
杨思倩 <siqian20150501@163.com>
2018-08-23 19:09:35 +0800

    显示当前新闻的评论数据

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -144,10 +144,23 @@ def news_detail(news_id):
         if news in g.user.collection_news:
             is_collected = True
 
+    # 查询评论数据
+    comments = []
+    try:
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    comment_dict_li = []
+    for comment in comments:
+        comment_dict_li.append(comment.to_dict())
+
+
     data = {
         "user":user.to_dict() if user else None,
         "news_dict_li":news_dict_li,
         "is_collected": is_collected,
-        "news":news.to_dict()
+        "news":news.to_dict(),
+        "comments":comment_dict_li
     }
     return render_template("news/detail.html",data=data)
