3eb41e2c51852a6e9832035a96954eece37c82a7
杨思倩 <siqian20150501@163.com>
2018-08-25 19:14:34 +0800

    基本资料修改

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,14 +1,30 @@
-from flask import render_template, g, redirect, request
+from flask import render_template, g, redirect, request, jsonify
 
 from info.modules.profile import profile_blue
+from info.utils.response_code import RET
 from info.utils.set_filters import user_login_data
 
 
 @profile_blue.route("/base_info",methods=["GET", "POST"])
 @user_login_data
 def base_info():
-    if request.method=="GET":
+    if request.method == "GET":
         return render_template("news/user_base_info.html", data={"user": g.user.to_dict()})
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
+
+    if not all([nick_name,signature,gender]):
+        return jsonify(errno=RET.PARAMERR,errmsg="参数错误")
+    if gender not in ("MAN", "WOMAN"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    user = g.user
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profile_blue.route("/info")
