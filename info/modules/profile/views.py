3d8739c3f05f5ec7ff9a7334a99d9d6f1b312dfb
杨思倩 <siqian20150501@163.com>
2018-08-25 15:50:58 +0800

    基本资料的界面展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,14 @@
-from flask import render_template, g, redirect
+from flask import render_template, g, redirect, request
 
 from info.modules.profile import profile_blue
 from info.utils.set_filters import user_login_data
+
+
+@profile_blue.route("/base_info",methods=["GET", "POST"])
+@user_login_data
+def base_info():
+    if request.method=="GET":
+        return render_template("news/user_base_info.html", data={"user": g.user.to_dict()})
 
 
 @profile_blue.route("/info")
