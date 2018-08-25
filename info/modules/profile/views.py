31e8b2c1954bc09d690e630cac2993b3281824b1
杨思倩 <siqian20150501@163.com>
2018-08-25 21:30:45 +0800

    修改密码的前后端逻辑实现

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,10 +1,39 @@
 from flask import render_template, g, redirect, request, jsonify, current_app
 
+from info import db
 from info.constants import QINIU_DOMIN_PREFIX
 from info.modules.profile import profile_blue
 from info.utils.image_storage import storage
 from info.utils.response_code import RET
 from info.utils.set_filters import user_login_data
+
+
+@profile_blue.route("/pass_info",methods=["GET", "POST"])
+@user_login_data
+def pass_info():
+    user = g.user
+    if request.method == "GET":
+        return render_template("news/user_pass_info.html", data={"user": user.to_dict()})
+
+    old_password = request.json.get("old_password")
+    new_password = request.json.get("new_password")
+
+    if not all([old_password,new_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    if not user.check_passowrd(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg="原密码错误")
+
+    user.password = new_password
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.DBERR, errmsg="数据保存失败")
+
+    return jsonify(errno=RET.OK, errmsg="保存成功")
+
 
 
 @profile_blue.route("/pic_info",methods=["GET", "POST"])
