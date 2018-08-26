37d6a35bbdfc7565fa84ae6f4d868285608a819c
杨思倩 <siqian20150501@163.com>
2018-08-26 09:41:09 +0800

    评论bug修复

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -65,6 +65,53 @@ def comment_like():
         return jsonify(errno=RET.DBERR, errmsg="数据库操作失败")
     return jsonify(errno=RET.OK, errmsg="操作成功")
 
+
+@news_blue.route('/news_comment', methods=["POST"])
+@user_login_data
+def add_news_comment():
+    """添加评论"""
+
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+    # 获取参数
+    data_dict = request.json
+    news_id = data_dict.get("news_id")
+    comment_str = data_dict.get("comment")
+    parent_id = data_dict.get("parent_id")
+
+    if not all([news_id, comment_str]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="该新闻不存在")
+
+    # 初始化模型，保存数据
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_str
+    if parent_id:
+        comment.parent_id = parent_id
+
+    # 保存到数据库
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="保存评论数据失败")
+
+    # 返回响应
+    return jsonify(errno=RET.OK, errmsg="评论成功", data=comment.to_dict())
+
+
 @news_blue.route("/news_collect", methods=['POST'])
 @user_login_data
 def news_collect():
